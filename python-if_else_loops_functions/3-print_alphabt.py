9737f1af4ce8fb6916fe810e446b995828b4e560
Fitsum Berhane <f.haile@alustudent.com>
2024-06-22 18:18:09 +0200

    fix on rangex

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')):
+for letter in range(ord('a'), ord('z')+1):
     if letter == ord('q') or letter == ord('e'):
         continue
     print("{:s}".format(chr(letter)), end="")
