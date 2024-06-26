cbb8059c128996ec8c754d993e58f49b64b7900a
Fitsum Berhane <f.haile@alustudent.com>
2024-06-26 21:00:30 +0200

    __main__ fix for script execution

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,7 @@ import sys
 
 
 number_arguments = len(sys.argv)
-print("{} arguments:".format(number_arguments -1))
-for i in range(1, number_arguments):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    print("{} arguments:".format(number_arguments -1))
+    for i in range(1, number_arguments):
+        print("{}: {}".format(i, sys.argv[i]))
