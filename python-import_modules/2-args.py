3f85dc03ecc51ca962553d168beddce53c17df91
Fitsum Berhane <f.haile@alustudent.com>
2024-06-26 21:20:52 +0200

    if statements for 0 and 1 arguments to meet the desired output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-
-number_arguments = len(sys.argv)
+number_arguments = len(sys.argv) - 1
 if __name__ == "__main__":
-    print("{} arguments:".format(number_arguments -1))
-    for i in range(1, number_arguments):
+    if number_arguments == 0:
+        print("0 arguments.")
+    elif number_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number_arguments))
+    for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
