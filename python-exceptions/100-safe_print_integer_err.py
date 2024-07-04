6b376ff252dd1c59c7298c55090b8cc6eaf3e461
Fitsum Berhane <f.haile@alustudent.com>
2024-07-04 11:44:38 +0200

    Fix: PEP8 validation

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
-
 import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
 
 if __name__ == "__main__":
-    safe_print_integer_err = __import__('100-safe_print_integer_err').safe_print_integer_err
-
     value = 89
     has_been_print = safe_print_integer_err(value)
     if not has_been_print:
