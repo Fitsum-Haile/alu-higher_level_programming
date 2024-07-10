2069d7e8617cb50b695ec26f3b533cdf299d24ce
Fitsum Berhane <f.haile@alustudent.com>
2024-07-10 13:48:46 +0200

    Fix: PEP8 validation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Square class definition with size validation, area calculation, printing capabilities, and position handling."""
+"""Square class definition with size validation, area calculation, 
+printing capabilities, and position handling."""
 
 
 class Square:
@@ -14,7 +15,8 @@ class Square:
             position (tuple): The position of the square. Defaults to (0, 0).
 
         Raises:
-            TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
+            TypeError: If size is not an integer or position is 
+            not a tuple of 2 positive integers.
             ValueError: If size is less than 0.
         """
         self.size = size
