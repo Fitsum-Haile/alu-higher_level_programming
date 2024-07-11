58920ab4627a1c52410e1e8265d1ca68c9664bad
Fitsum Berhane <f.haile@alustudent.com>
2024-07-11 14:34:16 +0200

    Fix: PEP8 validation

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join([str(self.print_symbol) * self.__width
-for _ in range(self.__height)])
+    for _ in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
