bf903c38d7620c7d6264c0a91390490407ca0af0
Fitsum Berhane <f.haile@alustudent.com>
2024-07-12 07:39:02 +0200

    additinal tasks

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""Defines a class LockedClass"""
+
 
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
 
-# Example Usage (101-main.py)
-if __name__ == "__main__":
-    LockedClass = __import__('101-locked_class').LockedClass
+    Attributes:
+        first_name (str): first name of something.
+    """
 
-    lc = LockedClass()
-    lc.first_name = "John"
-    try:
-        lc.last_name = "Snow"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instances of Locked Class."""
+
+        self.first_name = "first_name"
