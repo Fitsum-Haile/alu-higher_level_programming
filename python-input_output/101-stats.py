2ca651ab1bfb8d8b196aff6af7ab3daf273fc9db
Fitsum Berhane <f.haile@alustudent.com>
2024-07-18 18:37:51 +0200

    additional tasks

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -22,8 +22,10 @@ def print_statistics(total_size, status_counts):
 
 def main():
     total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0,\
-            403: 0, 404: 0, 405: 0, 500: 0}
+    status_counts = {
+    200: 0, 301: 0, 400: 0, 401: 0,
+    403: 0, 404: 0, 405: 0, 500: 0
+}
     line_count = 0
 
     try:
