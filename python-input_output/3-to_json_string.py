a5c03eb8b191a45654451f0cfa7aedd0dc34891e
Fitsum Berhane <f.haile@alustudent.com>
2024-07-18 12:23:48 +0200

    Fix: to_json_string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,10 +8,9 @@ import json
 
 
 def to_json_string(my_obj):
-     """Return the JSON representation of an object (string).
-
+    """Return the JSON representation of an object (string).
     Args:
-        my_obj (any): The object to convert to JSON string.
+    my_obj (any): The object to convert to JSON string.
 
     Returns:
         str: The JSON representation of the object.
