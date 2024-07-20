b19e3de1069344df774c56c1113fcf77294e3670
Fitsum Berhane <f.haile@alustudent.com>
2024-07-20 17:04:52 +0200

    trial

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 
-SELECT tv_genres.name 
+SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
