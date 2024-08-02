d0fe4e22bffdf796e5f0650ce26b9688c15011cf
Fitsum Berhane <f.haile@alustudent.com>
2024-08-02 11:18:34 +0200

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a custom header
+# and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
