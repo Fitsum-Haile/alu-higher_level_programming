9375470e74bfb6de6b40d6f88e1ecf4de69d69d9
Fitsum Berhane <f.haile@alustudent.com>
2024-08-02 13:41:03 +0200

    Fix: making executable

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,11 +4,14 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # Updated URL
+    url = "http://0.0.0.0:5050/status"  # Adjusted URL for local testing
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except Exception as e:
+        print(f"An error occurred: {e}")
