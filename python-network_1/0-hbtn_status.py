0c96ec95c55bc41353ae89b77a75164b0728087b
Fitsum Berhane <f.haile@alustudent.com>
2024-08-02 13:44:55 +0200

    Fix: making executable

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,11 +4,16 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"  # Correct URL as per requirement
+    url = "https://intranet.hbtn.io/status"  # Ensure this URL is correct
     request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print(f"HTTPError: {e.code} - {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URLError: {e.reason}")
