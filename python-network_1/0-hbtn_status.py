0eecaac24cb4380c538704b7b29b21ea2ceaabfb
Fitsum Berhane <f.haile@alustudent.com>
2024-08-04 17:36:32 +0200

    trial

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status and prints the response details."""
-
 import urllib.request
 
+
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"  # Ensure this URL is correct
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTPError: {e.code} - {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URLError: {e.reason}")
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
