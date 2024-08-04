82fa2313645ec53bef29a18e405d025758f6f180
Fitsum Berhane <f.haile@alustudent.com>
2024-08-04 17:51:34 +0200

    a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
-"""SENDS A POST REQUEST"""
+"""
+Sends a POST request to a specified URL with email data.
 
-import requests  # Replace urllib with requests
+This module takes two command-line arguments:
+1. URL of the target endpoint.
+2. Email address to be sent in the POST request.
+
+The module constructs a POST request with the provided email data, sends it to the specified URL,
+and prints the response body to the console.
+"""
+
+import sys
+import urllib.parse
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = {'email': sys.argv[2]}  # Use data instead of val for clarity
+    val = {'email': sys.argv[2]}
+    dta = urllib.parse.urlencode(val).encode("ascii")
 
-    # Use requests.post for simpler POST request
-    response = requests.post(url, data=data)
-
-    # Print the response content
-    print(response.text)
+    req = urllib.request.Request(url, dta)
+    with urllib.request.urlopen(req) as ans:
+        print(ans.read().decode("utf-8"))
