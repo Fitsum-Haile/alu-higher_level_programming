6b9fb92cb37ab8564f6203e825f5c4a3a4656c7c
Fitsum Berhane <f.haile@alustudent.com>
2024-08-07 15:02:36 +0200

    a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -9,20 +9,16 @@ if __name__ == "__main__":
     # Get the letter from command-line arguments or default to an empty string
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    
     # Send the POST request
     response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    
     try:
         # Try to parse the JSON response
         result = response.json()
-        
         if not result:
             print("No result")
         else:
             user_id = result.get("id")
             name = result.get("name")
-            
             if user_id is None or name is None:
                 print("No result")
             else:
