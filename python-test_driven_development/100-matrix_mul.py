15838a8b5317b8c4e7921c2562838a1d0b159c84
Fitsum Berhane <f.haile@alustudent.com>
2024-08-08 15:00:58 +0200

    trial

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -8,29 +8,24 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    
     if len(m_a) == 0 or len(m_a[0]) == 0:
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0 or len(m_b[0]) == 0:
         raise ValueError("m_b can't be empty")
-    
     for row in m_a:
         if not all(isinstance(element, (int, float)) for element in row):
             raise TypeError("m_a should contain only integers or floats")
     for row in m_b:
         if not all(isinstance(element, (int, float)) for element in row):
             raise TypeError("m_b should contain only integers or floats")
-    
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
-    
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     
@@ -43,5 +38,4 @@ def matrix_mul(m_a, m_b):
                 sum_product += m_a[i][k] * m_b[k][j]
             row.append(sum_product)
         result.append(row)
-    
     return result
