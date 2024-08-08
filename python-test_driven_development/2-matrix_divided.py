05559af586fc149ed8836e52060da8ff11664ac3
Fitsum Berhane <f.haile@alustudent.com>
2024-08-08 11:54:07 +0200

    new

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,24 +6,28 @@ by a number and rounds to 2 decimal places.
 """
 
 
+
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix
     Args:
         matrix (list): list of lists of integers or floats
-        div (int): number to divide matrix by
+        div (int or float): number to divide matrix by
     Returns:
         list: new matrix
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for item in row:
             if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
