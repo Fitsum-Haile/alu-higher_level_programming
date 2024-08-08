5722e8917c932f842292aab8827626518f61379f
Fitsum Berhane <f.haile@alustudent.com>
2024-08-08 12:54:30 +0200

    a function that prints a text with 2 new lines after each of these characters: ., ? and :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,16 +5,19 @@ text with 2 new lines after each of these characters: ., ? and :.
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after
-    each of these characters: ., ? and :."""
+    """Prints a text with 2 new lines after each
+    of these characters: ., ? and :.
+
+    Args:
+        text (str): The text to be printed.
+
+    Raises:
+        TypeError: If `text` is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    punctuation = ['.', '?', ':']
-    result = ""
+
     for char in text:
-        result += char
-        if char in punctuation:
-            result += "\n\n"
-    
-    print(result.strip())
+        print(char, end="")
+        if char in ".?:":
+            print("\n")
