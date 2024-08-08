bd88ee956a6e975932ed83f69fd7bb69804486d0
Fitsum Berhane <f.haile@alustudent.com>
2024-08-08 19:28:17 +0200

    almost a circle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,18 +2,16 @@
 '''
 this module contains a function that prints a text with 2 new lines after each of these characters: ., ? and :
 '''
+
+
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :
-    Args:
-        text (str): text to print
-    Raises:
-        TypeError: text must be a string
-    """
-    if type(text) is not str:
+    '''Fucntion to bring the texts'''
+    if type(text) != str:
         raise TypeError("text must be a string")
-    for i in range(0, len(text)):
-        print(text[i], end="")
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print("\n")
-            if i < len(text) - 1 and text[i + 1] == " ":
-                i += 1
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)]
+        )
+
+    print("{}".format(text), end="")
