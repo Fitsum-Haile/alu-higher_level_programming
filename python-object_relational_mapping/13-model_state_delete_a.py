c25236e37435d360df85542e1d8020f905d43c87
Fitsum Berhane <f.haile@alustudent.com>
2024-08-09 13:07:37 +0200

    trial

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa
+the letter 'a' from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -9,19 +9,36 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./13-model_state_delete_a.py <mysql username> "
+              "<mysql password> <database name>")
+        return
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost/{database}",
+            pool_pre_ping=True
+        )
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Query and delete states
+        states_to_delete = session.query(State).filter(
+            State.name.contains('a')
+        ).all()
+        for state in states_to_delete:
+            session.delete(state)
+
+        session.commit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        session.close()
+
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base.metadata.create_all(engine)
-
-    states = session.query(State).filter(State.name.contains('a')).all()
-
-    for state in states:
-        session.delete(state)
-
-    session.commit()
-
-    session.close()
+    main()
