89a4f0a29ef13c9871565c6a1f251dc20be123c1
Fitsum Berhane <f.haile@alustudent.com>
2024-09-23 18:31:06 +0200

    semistandard validation

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,8 +4,8 @@
 const argNumber = process.argv.length - 2;
 if (argNumber === 0) {
   console.log('No argument');
-}else if (argNumber === 1) {
+} else if (argNumber === 1) {
   console.log('Argument found');
-}else {
+} else {
   console.log('Arguments found');
 }
