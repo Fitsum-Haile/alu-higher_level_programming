01d9684cefe263dac365ba4eff5894fce69a03ca
Fitsum Berhane <f.haile@alustudent.com>
2024-10-03 14:57:21 +0200

    Fix character order and handle missing names in Star Wars API script

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(movieUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else if (response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
+request(process.argv[2], (err, res, body) => {
+  if (!err) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    tasks.forEach(task => {
+      if (task.completed && completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else if (task.completed) {
+        completed[task.userId] += 1;
+      }
     });
-  } else {
-    console.log(`Error: ${response.statusCode}`);
+    console.log(completed);
   }
 });
